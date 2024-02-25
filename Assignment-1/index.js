2c96de99657a0f6dffd73317976c7ef1dbd6ede3
Verdan Kumar <vardantharwani@gmail.com>
2024-02-26 02:04:47 +0500

    Adding First Assignment

---
+++ Assignment-1/index.js
@@ -0,0 +1,127 @@
+
+// Chapter 1
+
+
+// Question 1
+
+// Write a script to greet your website visitor using JS alert box.
+
+// alert('Hello')
+
+
+
+// Question 2
+
+//  Write a script to display following message on your webpage:
+
+// alert('Error! Please enter a valid password.')
+
+
+
+// Question 3
+
+// Write a script to display following message on your webpage: (Hint : Use line break)
+
+// alert("Welcome to JS Land...\nHappy coding."
+// )
+
+
+
+// Question 4
+
+// Write a script to display following messages in sequence:
+
+// alert("Welcome to JS Land....")
+// alert("Happy coding.\n<input type='checkbox'>Prevent this page from creating additional dialogues</input>")
+
+
+
+
+
+
+
+
+
+
+// CHAPTER 2
+
+// Question 1
+
+// Declare a variable called username.
+
+// var username;
+
+
+
+
+// Question 2
+
+// Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// var myName = "Verdan Kumar"
+
+
+
+// Question 3
+
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+
+// var message = 'Hello World'
+// alert(message) 
+
+
+
+// Question 4
+
+// Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// var name = "Verdan Kumar"
+// var age = "25"
+// var certificate = "Web and mobile app development."
+
+// alert(name)
+// alert(age)
+// alert(certificate)
+
+
+//  Question 5 
+
+// Write a script to display the following alert using one JS variable:
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+
+
+
+// Question 6
+
+//  Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+// var email = "vardantharwani@gmail.com"
+// alert(`my email is ${email}`)
+
+
+
+// Question 7 
+
+// Declare a variable called book & give it the value “Asmarter way to learn JavaScript”. Display the following message in an alert box:
+
+
+// var book = 'A smarter way to learn JavaScript'
+// alert('I am trying to learn from the book ' + book)
+
+
+
+
+// Question 9
+
+// Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design)
