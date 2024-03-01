2a81beff133f3f58771b0df4becd2b1a18e6d717
Verdan Kumar <vardantharwani@gmail.com>
2024-03-01 23:53:10 +0500

    Adding Second JS Assignment

---
+++ Assignment-2/app.js
@@ -0,0 +1,149 @@
+// Assignment 2
+
+// Chapter 4
+
+// Q2 Declare 5 legal & 5 illegal variable names.
+
+// legal variable names
+
+// let legal;
+// let illegal;
+// let school;
+// let schoolling;
+// let student;
+
+
+// Illegal variable names
+
+// let var;
+// let name;
+// let alert;
+// let class;
+// let function;
+
+
+
+// Q3 
+
+// Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write(`Variable names can only contain . numbers, $ and _ . For example : $my_1stVariable`)
+// document.write(`Variable names must begin with a letter, $ or _ . For example : $name, _name or name`)
+// document.write('Variable names are case sensitive')
+// document.write('Variable names should be not JS keywords')
+
+
+
+// Chapter 5
+
+// Q1 Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+// const a = 3
+// const b = 5
+// const sum = a + b
+
+
+// document.write("Sum of " + a  +" and " + b +" is "+ sum)
+
+// Q2 Repeat task1 for subtraction, multiplication, division &
+// modulus.
+
+// const difference = a - b
+// const multiplication = a * b
+// const division = a / b
+// const modulus = a % b
+
+// document.write("Subtraction of " + a  +" and " + b +" is "+ difference)
+// document.write("Multiplication of " + a  +" and " + b +" is "+ multiplication)
+// document.write("Division of " + a  +" and " + b +" is "+ division)
+// document.write("Modulus of " + a  +" and " + b +" is "+ modulus)
+
+
+
+//  Q3 Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value
+
+// after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”.
+
+
+
+
+// let variable;
+
+// document.write(`<h2>Value of variable declaration is ${variable}</h2>`)
+
+
+// variable = 1
+
+// document.write(`<h3>Initial value : ${variable}</h3>`)
+
+// variable++
+
+
+// document.write(`<h3>Value after increment is: ${variable}</h3>`)
+
+// variable += 7
+
+// document.write(`<h3>Value after addition is: ${variable}</h3>`)
+
+// variable--
+
+// document.write(`<h3>Value after decrement is: ${variable}</h3>`)
+
+// variable /= 3
+
+// document.write(`<h3>Output: The reminder is: ${variable}</h3>`)
+
+
+
+
+
+
+
+
+
+//  Q3 Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+
+
+
+
+let ticketPrice = 600;
+let totalTickets = 5
+
+let totalCost = totalTickets * ticketPrice;
+
+document.write(`<h3>Total cost to buy ${totalTickets} to a movie is ${totalCost}PKR.</h3>`)
+
+
+
+
