0965bf73295f7a2361ca6f3bfe57c37ded58f549
verdan kumar <vardantharwani@gmail.com>
2024-07-17 01:17:58 +0500

    Adding chapter 6 to 9

---
+++ Exercises/Chapter 6-9/main.js
@@ -0,0 +1,93 @@
+// -------------------- MATH EXPRESSIONS ----------------------
+
+
+// Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+
+// UNCOMMENT THE CODE BELOW
+
+
+
+// var num = 10;
+
+// document.write('Result'+"<br>")
+// document.write('The value of num is '+ num+"<br>")
+
+// document.write("-----------------------------------<br>")
+
+// document.write('The value of ++num is '+ ++num+"<br>")
+// document.write('now the value of num is '+ num+"<br>")
+
+// document.write('<br>')
+// document.write('<br>')
+
+// document.write('The value of num++ is '+ (num++)+"<br>")
+// document.write('now the value of num is '+ num+"<br>")
+
+// document.write('<br>')
+// document.write('<br>')
+
+// document.write('The value of --num is '+ (--num)+"<br>")
+// document.write('now the value of num is '+ num+"<br>")
+
+
+
+// document.write('<br>')
+// document.write('<br>')
+
+// document.write('The value of num-- is '+ (num--)+"<br>")
+// document.write('now the value of num is '+ num+"<br>")
+
+
+
+
+
+// Write a program that takes input a name from user &
+// greet the user.
+
+
+// UNCOMMENT THE CODE BELOW
+
+
+
+
+// let name = prompt('Enter your name')
+// alert('Hello '+ name)
+
+
+
+
+
+
+
+// Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+
+// UNCOMMENT THE CODE BELOW
+
+
+
+// let table = prompt('Enter a number for printing the table', 5)
+// let num = 0
+// table = Number(table)
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+// document.write(table + "X" + ++num + "=" + table*num + "<br>")
+
+
+
+
+
+
+
+
