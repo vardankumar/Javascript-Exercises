df03f9526f1bf9931dc9519e2d3d64acb694cd42
verdan kumar <vardantharwani@gmail.com>
2024-07-17 03:27:21 +0500

    adding chapters from 9 to 11

---
+++ Exercises/Chapter 9-11/main.js
@@ -0,0 +1,157 @@
+//  --------------------- USER INPUT & CONDITIONAL STATEMENT ---------------------
+
+
+
+
+
+
+// Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+
+// UNCOMMENT THE CODE BELOW
+
+// var city = prompt('Enter your city')
+// if(city.toLowerCase() === 'karachi'){
+//     alert('Welcome to the city of lights')
+// }
+
+
+//  --------------------------------------------------------
+
+
+
+
+// Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+// UNCOMMENT THE CODE BELOW
+
+
+
+// let gender = prompt('Enter your gender please.')
+// if(gender.toLowerCase() === 'male'){
+//     alert('Good Morning Sir')
+// }else if(gender.toLowerCase() === 'female'){
+//     alert('Good Morning Maam')
+// }
+
+
+
+
+
+// ------------------------------------------------------
+
+// . Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+
+// UNCOMMENT THE CODE BELOW
+
+
+
+// let color = prompt('Enter the traffic light color')
+// if(color.toLowerCase() === 'red'){
+//     alert('Must Stop')
+// }
+// else if(color.toLowerCase() === 'yellow'){
+//     alert('Ready To Move')
+// }
+// else if(color.toLowerCase() === 'green'){
+//     alert('Move Now')
+// }
+
+
+
+
+
+
+// ------------------------------------------------------
+
+// Write a program to take input remaining fuel in car (in
+//     litres) from user. If the current fuel is less than 0.25litres,
+//     show the message “Please refill the fuel in your car
+
+
+// UNCOMMENT THE CODE BELOW
+
+// let fuel = prompt('Enter the available fuel in your car')
+// if(fuel < 0.25){
+//     Alert('Please refill the fuel in your car.')
+// }
+
+
+// -------------------------------------------------------
+
+
+
+
+// Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+
+
+// UNCOMMENT THE CODE BELOW
+
+
+
+// a. 
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+// // b. 
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// // c.
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+// // d. 
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// // e. 
+// if (true){
+// alert("True")
+// }
+// if (false){
+// alert("False");
+// }
+
+
+// // f. 
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+    
+
+// -------------------------------------------------------
+
+
+
+
+
+
