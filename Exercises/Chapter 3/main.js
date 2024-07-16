02381a058ccf88b5a802a6a779f852a814fff587
verdan kumar <vardantharwani@gmail.com>
2024-07-16 22:56:28 +0500

    adding chapter 3 of javascript exercises

---
+++ Exercises/Chapter 3/main.js
@@ -0,0 +1,25 @@
+// Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthYear = 1998
+
+// document.write("My birth year is "+birthYear+"<br/>"+"data type of my declared variable is"+typeof birthYear)
+
+
+
+
+
+// A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+var vName = 'Bill Gates'
+var product = 'Macbook'
+var quantity = 12
+document.write(vName + " ordered " + quantity + " " + product + "(s) on Amazon.com.")
