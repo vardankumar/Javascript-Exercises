aa47e6ddd2edca6b1bca4cf055e7661b3ca125b3
verdan kumar <vardantharwani@gmail.com>
2024-07-17 15:26:20 +0500

    adding chapter 26 to 30

---
+++ Exercises/Chapter 21-25/main.js
@@ -0,0 +1,12 @@
+//  ------------- STRING METHODS  -----------------
+
+// Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.
+
+
+let firstname = prompt('Enter your first name')  
+let lastname = prompt('Enter your last name')  
+let fullName = firstname + " " + lastname;
+
+alert('Greetings, '+ fullName)
---
+++ Exercises/Chapter 26-30/main.js
@@ -0,0 +1,45 @@
+//  --------------- MATH METHODS  -------------
+
+
+// Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+
+// let num = prompt('Enter a number')
+// document.write('number : '+ num + "<br>")
+
+// // b. round off value of the number
+// document.write('round off value : ' + Math.round(num) + "<br>")
+
+// // c. floor value of the number
+// document.write('floor value : ' + Math.floor(num) + "<br>")
+
+
+// // d. ceil value of the number
+// document.write('Ceil value : ' + Math.ceil(num) + "<br>")
+
+
+
+
+// ---------------------------------------------
+
+
+
+
+// Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+
+
+// let random = Math.floor(Math.random() * 6) + 1
+// document.write('Random dice value : '+ random)
+
+
+
+
+// ---------------------------------------------
+
+// . Write a program that shows a random number between 1
+// and 100 in your browser.
+
+document.write("Random value between 1 and 100 : " + (Math.floor(Math.random()* 100) + 1))
